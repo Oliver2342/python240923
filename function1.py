25cc7f7c19fc7a60dfbd6db3aac245c62f9b825f
jagerbomb55@gmail.com <jagerbomb55@gmail.com>
2024-09-23 16:23:39 +0900

    연습2

--- function1.py
+++ function1.py
@@ -32,3 +32,33 @@ def intersect(prelist, postlist):
 
 #호출
 print(intersect("HAM","SPAM"))
+
+
+
+#기본값을 명시
+def times(a=10, b=20):
+    return a*b
+
+#호출
+print(times())
+print(times(5))
+print(times(5,6))
+
+#지역 변수와 전역 변수
+
+x = 5
+def func1(a):
+    return a+x
+
+
+#호출
+print(func1(1))
+
+
+def func2(a):
+    #지역변수
+    x = 1
+    return a+x
+
+#호출
+print(func2(1))
